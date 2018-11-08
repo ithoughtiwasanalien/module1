bd4fec8cbf27417beb552bc74955970469bde740
Daria <daryakasyan@gmail.com>
2018-11-08 19:53:20 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,35 @@
+// // //var x = +prompt("vvedite chislo");
+
+
+
+// // // (x <= 100) {
+// // // alert("znachenie  menshe 100");
+
+// // //} else {
+// // //  alert('zhachenie bolshe 100');
+
+// // //}
+
+// // var x = +prompt("введите число якоето от 1-3");
+
+
+// // if (x == 1) {
+// //     alert("вы ввели 1");
+
+
+// // } else if (x == 2) {
+// //     alert("вы ввели 2");
+// // } else if (x == 3) {
+// //     alert("вы ввели 3");
+
+// // }
+
+// var age = +prompt('age');
+
+// age >= 18 ? alert("Доступ открыт") : alert("Доступ закрыт");
+
+var x = document.getElementsById('polychisya');
+
+x.addEventListener('mouseover', function() {
+    this.style.color = "red";
+});
